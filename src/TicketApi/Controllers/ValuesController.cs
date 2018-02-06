f63fa3f7abb0f40317f13a80f1c9d46edc23b38b
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-02-06 14:55:07 +0100

    labb cod

--- src/TicketApi/Controllers/ValuesController.cs
+++ src/TicketApi/Controllers/ValuesController.cs
@@ -9,36 +9,59 @@ namespace TicketApi.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private static List<string> test;
+        public ValuesController()
+        {
+            if (test == null)
+            {
+                test = new List<string>();
+            }
+        }
         // GET api/values //
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return test;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            try
+            {
+                
+                return test[id];
+            }
+            catch
+            {
+                return "Namnet finns ej";
+            }
         }
 
         // POST api/values
+       
         [HttpPost]
         public void Post([FromBody]string value)
         {
+          
+            test.Add(value);
+          
+
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            test[id] = value;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            test.RemoveAt(id);
         }
     }
 }
