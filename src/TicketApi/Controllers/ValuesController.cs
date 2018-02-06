ec1d6e845db742bfc4fb6f57cfc86e71b8a6bfb4
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-02-06 13:09:45 +0100

    test

--- src/TicketApi/Controllers/ValuesController.cs
+++ src/TicketApi/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace TicketApi.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        // GET api/values //
         [HttpGet]
         public IEnumerable<string> Get()
         {
