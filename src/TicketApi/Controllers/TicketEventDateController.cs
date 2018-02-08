33a8b249c589b357db7b8b3d9569a87e6827920a
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-02-08 18:47:05 +0100

    Lagt till Två Controllers

--- src/TicketApi/Controllers/TicketEventDateController.cs
+++ src/TicketApi/Controllers/TicketEventDateController.cs
@@ -4,36 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using TicketSystem.DatabaseRepository.Model;
 
 namespace TicketApi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Ticket")]
-    public class TicketEventController : Controller
+    [Route("api/TicketEventDate")]
+    public class TicketEventDateController : Controller
     {
-        // GET: api/Ticket
+        // GET: api/TicketEventDate
         [HttpGet]
-        //public List<TicketEvent> Get()
-        //{
-        //    return;
-        //}
-       ///
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-        // GET: api/Ticket/5
+        // GET: api/TicketEventDate/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
         
-        // POST: api/Ticket
+        // POST: api/TicketEventDate
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
         
-        // PUT: api/Ticket/5
+        // PUT: api/TicketEventDate/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
