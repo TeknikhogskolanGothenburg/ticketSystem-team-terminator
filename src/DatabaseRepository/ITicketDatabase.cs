5e127b523b14f38b3deb81a590a10d802f49e8f2
Kull1z <32952194+Kull1z@users.noreply.github.com>
2018-02-09 18:00:37 +0100

    TicketEventDate Controller update, also starting on TicketEventDate Methods

--- src/DatabaseRepository/ITicketDatabase.cs
+++ src/DatabaseRepository/ITicketDatabase.cs
@@ -14,9 +14,13 @@ namespace TicketSystem.DatabaseRepository
         /// <returns>An object representing the newly created TicketEvent</returns>
         TicketEvent EventAdd(string name, string description);
 
+
+   
         List<TicketEvent> AllEvents();
         List<TicketEvent> EventsFind(string query);
         // Slut Adriana
+
+      
 
 
         /// <summary>
@@ -36,11 +40,33 @@ namespace TicketSystem.DatabaseRepository
         /// <param name="query">A text which is user i looking for in the venues</param>
         /// <returns>A list of venus matching the query</returns>
         List<Venue> VenuesFind(string query);
-
+       
         List<Venue> VenuesAll();
 
         void VenueUpdate(Venue venue);
 
         void VenueDelete(int id);
+
+        //Joakim testing testing
+
+        List<TicketEventDate> TicketEventDateFind(string query);
+        List<TicketEventDate> TicketEventDateAll();
+
+        /// <summary>
+        /// Add a new ticketeventdate to the database
+        /// </summary>
+        /// <param name="ticketEventDateId">Name of the event</param>
+        /// <param name="ticketEventId">Physical address of the venue</param>
+        /// <param name="venueId">City part of the adress</param>
+        /// <param name="eventStartDateTime">Country part of the adress</param>
+        /// <returns>An object representing the newly created TicketEventDate</returns>
+     
+        TicketEventDate TicketEventDateAdd(int ticketEventDateId, int ticketEventId, int venueId, time eventStartDateTime);
+
+
+        void TicketEventDateUpdate(TicketEventDate ticketEventDate);
+
+        void TicketEventDateDelete(int id);
+        // Joakim Testar
     }
 }
