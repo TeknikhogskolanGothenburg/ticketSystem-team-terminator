81b073204ff77a6c3c3759ea09804ddd47d2691b
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2018-02-13 12:00:02 +0100

    Classer på TicketsToTransactions & SeatAtEventDate

---
+++ src/DatabaseRepository/Model/SeatsAtEventDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystem.DatabaseRepository.Model
+{
+    
+    class SeataAtEventDate
+    {
+       public int SeatID { get; set; }
+            
+       public int TicketEventDateID { get; set; }
+            
+        }
+    }
+
