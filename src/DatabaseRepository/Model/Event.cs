0cee4d2575c4d15710adc50f3ca054e64f7d84f3
AngelaFang028 <yoyohui21@gmail.com>
2018-02-14 01:41:07 +0100

    Validation

--- src/DatabaseRepository/Model/Event.cs
+++ src/DatabaseRepository/Model/Event.cs
@@ -5,9 +5,11 @@ using System.Text;
 
 namespace TicketSystem.DatabaseRepository.Model
 {
-  public  class Event
-    {   [Required]
+    public  class Event
+    {
+        [Required]
         public Venue Venues { get; set; }
+        
         public int Seats { get; set; }
         [Required]
         public  TicketEvent TicketEvents { get; set; }
--- src/DatabaseRepository/Model/TicketTransaction.cs
+++ src/DatabaseRepository/Model/TicketTransaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TicketSystem.DatabaseRepository.Model
@@ -7,8 +8,13 @@ namespace TicketSystem.DatabaseRepository.Model
     public class TicketTransaction
     {
         public int TransactionID { get; set; }
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$")]
         public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$")]
         public string FirstName { get; set; }
+        [Required]
         public string Address { get; set; }
         public string City { get; set; }
         public string PaymentStatus { get; set; }
