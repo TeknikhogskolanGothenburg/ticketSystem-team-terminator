1bd527bde882d1e0d22ffec97aed4b0a5b5e5dc5
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2018-02-17 18:00:50 +0100

    MailSender

---
+++ src/TicketShopWebApplication/MailSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Mail;
+namespace TicketShopWebApplication
+{
+
+
+        public class MailSender
+        {
+
+            public void SendEmail(string from, string to, string subject, string body)
+            {
+                if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to))
+                {
+                    return;
+                }
+                SmtpClient client = new SmtpClient();
+                MailMessage message = new MailMessage();
+                message.Body = body;
+                message.Sender = new MailAddress(from);
+                message.To.Add(to);
+                message.Subject = subject;
+
+                client.Send(message);
+            }
+        }
+}
