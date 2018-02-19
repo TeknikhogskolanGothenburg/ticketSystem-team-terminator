1db9c91f2d29b1d405b313efc53d35edc6ad83d1
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2018-02-19 13:02:07 +0100

    mailhandler

---
+++ src/MailSender/MailHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    public class MailHandler
+    {
+  
+        
+        public static void SendEmail(string host, int port, string pasword, string from, string to, string subject, string body)
+        {
+            // ta bort dessa om vi har andra, hårdkodade för nu.
+            host = "smtp.gmail.com";
+            port = 587;
+
+            if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to))
+            {
+                return;
+            }
+            SmtpClient client = new SmtpClient();
+            MailMessage message = new MailMessage();
+            message.Body = body;
+            client.Host = host;
+            message.From = new MailAddress(from);
+            message.Sender = new MailAddress(from);
+            message.To.Add(to);
+            message.Subject = subject;
+            client.EnableSsl = true;
+            client.Port = port;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(from, pasword);
+            
+            client.Send(message);
+        }
+    }
+}
